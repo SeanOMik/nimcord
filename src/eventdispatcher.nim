44aee846b1785031f09bb7ed2bdbf1e62b07c2fe
SeanOMik <seanomik@gmail.com>
2020-05-31 00:09:30 -0500

    Create message create event.

--- src/eventdispatcher.nim
+++ src/eventdispatcher.nim
@@ -1,11 +1,17 @@
-import eventhandler, json, tables, hashes
+import eventhandler, json, tables, hashes, message
 
 proc readyEvent(json: JsonNode) =
     let readyEvent = ReadyEvent(readyPayload: json, name: $EventType.evtReady)
     dispatchEvent(readyEvent)
 
+proc messageCreateEvent(json: JsonNode) =
+    let msg = newMessage(json)
+    let messageCreateEvnt = MessageCreateEvent(message: msg)
+    dispatchEvent(messageCreateEvnt)
+
 let internalEventTable: Table[string, proc(json: JsonNode) {.nimcall.}] = {
-        "READY": readyEvent
+        "READY": readyEvent,
+        "MESSAGE_CREATE": messageCreateEvent
     }.toTable
 
 proc handleDiscordEvent*(json: JsonNode, eventName: string) =
--- src/eventhandler.nim
+++ src/eventhandler.nim
@@ -1,13 +1,16 @@
-import tables, hashes, json
+import tables, hashes, json, message
 
 type
     EventType* = enum
         evtReady = "READY"
+        evtMessageCreate = "MESSAGE_CREATE"
 
     BaseEvent* = object of RootObj
         name*: string
     ReadyEvent* = object of BaseEvent
         readyPayload*: JsonNode
+    MessageCreateEvent* = object of BaseEvent
+        message*: Message
 
 # Table storing all the event listeners
 let eventListeners = newTable[string, seq[proc(event: BaseEvent)]]()
