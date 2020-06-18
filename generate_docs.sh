e153ddea49733781d0dd8f92ad487de75704836a
SeanOMik <seanomik@gmail.com>
2020-06-18 16:09:22 -0500

    Make generate doc scripts better

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,1 +1,13 @@
-nim doc -o:docs/ --project src/message.nim
+nim doc -o:docs/Cache.html src/cache.nim
+nim doc -o:docs/Channel.html src/channel.nim
+nim doc -o:docs/Client.html src/client.nim
+nim doc -o:docs/ClientObjects.html src/clientobjects.nim
+nim doc -o:docs/DiscordObject.html src/discordobject.nim
+nim doc -o:docs/Emoji.html src/emoji.nim
+nim doc -o:docs/EventDispatcher.html src/eventdispatcher.nim
+nim doc -o:docs/EventHandler.html src/eventhandler.nim
+nim doc -o:docs/Guild.html src/guild.nim
+nim doc -o:docs/Member.html src/member.nim
+nim doc -o:docs/Message.html src/message.nim
+nim doc -o:docs/Nimcordutils.html src/nimcordutils.nim
+nim doc -o:docs/User.html src/user.nim
